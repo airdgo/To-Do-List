73a6e3bc61a443a68a256a5514b1b8d0ec7a3c29
airdgo <vladdragoi1@gmail.com>
2021-09-27 13:27:36 +0300

    improved local storage

--- script.js
+++ script.js
@@ -1,92 +1,104 @@
-const input = document.querySelector('[data-input]');
-const submit = document.querySelector('[data-submit]');
-const clearList = document.querySelector('[data-clear-list]');
+// Select the items
+const clear = document.querySelector("[data-clear-list]");
+const list = document.querySelector("[data-list]");
+const input = document.querySelector("[data-input]");
+const submit = document.querySelector("[data-submit]");
 
+// Clases names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST = [],
+    id = 0;
+
+// Get items from local storage
+let data = localStorage.getItem("TODO");
+
+// check if data is not empty
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+} else {
+    LIST = [];
+    id = 0;
+}
+
+// load items to the user interface
+function loadList(array) {
+    array.forEach(element => {
+        addToDo(element.name, element.id, element.done, element.trash);
+    });
+}
+
+// Add to do function
+function addToDo(toDo, id, done, trash) {
+
+    if (trash) { return; };
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `
+                <li class="list-item">
+                    <p class="list-item-paragraph ${LINE}">${toDo}</p>
+                    <i class="fa ${DONE} co checkbox" job="complete" id="${id}"></i>
+                    <i class="fas fa-trash-alt remove-button" job="delete" id="${id}"></i>
+                </li>
+                `;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+
+// Add an item when user hits submit
 submit.addEventListener('click', e => {
     e.preventDefault();
-
-    // Add input to local storage
-    const toDoList = localStorage.getItem('toDoList');
-    if (toDoList == null) {
-        toDoListObj = [];
-    } else {
-        toDoListObj = JSON.parse(toDoList);
-    };
-
-    myObj = {
-        input: input.value,
-        isCompleted: false
-    };
-
-    toDoListObj.push(myObj);
-    localStorage.setItem('toDoList', JSON.stringify(toDoListObj));
-    input.value = '';
-
-    showToDoList();
+    const toDo = input.value;
+    if (toDo) {
+        addToDo(toDo, id, false, false);
+        LIST.push({
+            name: toDo,
+            id: id,
+            done: false,
+            trash: false
+        });
+        // Add items to local storage
+        localStorage.setItem("TODO", JSON.stringify(LIST));
+        id++;
+    }
+    input.value = "";
 })
 
-// Take user input from local storage and display it on the page
-const showToDoList = () => {
-    const toDoList = localStorage.getItem('toDoList');
-    if (toDoList == null) {
-        toDoListObj = [];
-    } else {
-        toDoListObj = JSON.parse(toDoList);
+// Complete to do function
+const completeToDo = (element) => {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".list-item-paragraph").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+};
+
+const removeToDo = (element) => {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+};
+
+// Target items created dynamically
+list.addEventListener('click', e => {
+    const element = e.target;
+    const elementJob = element.attributes.job.value; // complete or delete
+
+    if (elementJob === "complete") {
+        completeToDo(element);
+    } else if (elementJob === "delete") {
+        removeToDo(element);
     };
+    // Add items to local storage
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
 
-    html = '';
-    toDoListObj.forEach((element, index) => {
-        html += `
-            <li class="list-item">
-                <p id="${index}" class="list-item-paragraph">${element.input}</p>
-                <button id="${index}" class="checkbox" onclick="checkNote(this.id)"><i class="fas fa-check-circle"></i></button>
-                <button id="${index}" class="remove-button" onclick="removeNote(this.id)"><i class="fas fa-trash-alt"></i></button>
-            </li>
-        `
-    });
-
-    let list = document.querySelector('[data-list]');
-
-    if (toDoListObj != 0) {
-        list.innerHTML = html;
-    } else {
-        list.innerHTML = `<li>Add a to do</li>`
-    }
-
-}
-
-// Clear all notes
-clearList.onclick = () => {
+// Clear the list
+document.querySelector("[data-clear-list]").onclick = () => {
     localStorage.clear();
 }
-
-let toDoList = localStorage.getItem('toDoList');
-
-// Check if note is checked
-const checkNote = (index) => {
-    toDoList = localStorage.getItem('toDoList');
-    if (toDoList == null) {
-        toDoListObj = [];
-    } else {
-        toDoListObj = JSON.parse(toDoList);
-    };
-
-
-    localStorage.setItem('toDoList', JSON.stringify(toDoListObj));
-}
-
-
-// Remove a note
-const removeNote = (index) => {
-    const toDoList = localStorage.getItem('toDoList');
-    if (toDoList == null) {
-        toDoListObj = [];
-    } else {
-        toDoListObj = JSON.parse(toDoList);
-    };
-    toDoListObj.splice(index, 1);
-    localStorage.setItem('toDoList', JSON.stringify(toDoListObj));
-    location.reload();
-}
-
-showToDoList();
